afb1fdc1f5a879eb37254ce45590be1970c708a3
w01f-o <arsenimarchenko@bk.ru>
2024-11-12 22:23:07 +0300

    fix: Changed origin in cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NextFunction, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use((_req, res: Response, next: NextFunction) => {
+    res.setHeader(
+      'Content-Security-Policy',
+      "connect-src 'self' https://w01f-server.ru:8080",
+    );
+    next();
+  });
 
   app.setGlobalPrefix('v1');
   app.useGlobalPipes(new ValidationPipe());
